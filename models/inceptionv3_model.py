df0da53d92ae49f22a7546d75c9b3dadd41a8165
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-03-14 12:56:36 +0530

    Add InceptionV3 model building function

--- models/inceptionv3_model.py
+++ models/inceptionv3_model.py
@@ -1,1 +1,40 @@
 
+import tensorflow as tf
+from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense, Dropout
+from tensorflow.keras.models import Model
+
+
+def build_inceptionv3():
+
+    base_model = InceptionV3(
+        weights="imagenet",
+        include_top=False,
+        input_shape=(299,299,3)
+    )
+
+    for layer in base_model.layers:
+        layer.trainable = False
+
+    x = base_model.output
+
+    x = GlobalAveragePooling2D()(x)
+
+    x = Dropout(0.5)(x)
+
+    outputs = Dense(1, activation="sigmoid")(x)
+
+    model = Model(inputs=base_model.input, outputs=outputs)
+
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001),
+        loss="binary_crossentropy",
+        metrics=[
+            "accuracy",
+            tf.keras.metrics.Precision(),
+            tf.keras.metrics.Recall(),
+            tf.keras.metrics.AUC()
+        ]
+    )
+
+    return model
