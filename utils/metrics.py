29623d12afcc9f1e58f1f98200a86db0e80a9f7e
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-03-14 12:25:37 +0530

    Update metrics.py

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,1 +1,11 @@
 
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+def evaluate(y_true, y_pred):
+
+    acc = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+
+    return acc, precision, recall, f1
