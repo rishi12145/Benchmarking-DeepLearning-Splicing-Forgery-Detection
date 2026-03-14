8356a5aff90ce63a0d19cc3cbc544f4a880fbcd6
Rishikesh Gopal <152694330+rishi12145@users.noreply.github.com>
2026-03-14 12:25:12 +0530

    Add image preprocessing function with resizing

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,11 @@
+import cv2
+import numpy as np
 
+def preprocess_image(image_path, size):
+
+    img = cv2.imread(image_path)
+    img = cv2.resize(img, size)
+
+    img = img / 255.0
+
+    return img
